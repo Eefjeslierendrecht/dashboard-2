c636f24fe57377e6ae570a355e874ff70c4b2004
Eefjeslierendrecht <eefjeslierendrecht@gmail.com>
2020-11-01 17:42:14 +0100

    dashboard3

---
+++ src/pages/StudentPage.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ChartComponent from "../components/ChartComponent";
+
+class StudentPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      studentName: props.match.params.name,
+    };
+  }
+
+  calculateAverageAssignment(studentData, assignment) {
+    let average = 0;
+    let items = 0;
+    studentData.forEach((item) => {
+      if (item.assignment === assignment) {
+        average += (item.difficult + item.fun) / 2;
+        items++;
+      }
+    });
+    return /*Math.round(*/ average / items; /*)*/
+  }
+
+  calculateAverageForOneStudent(studentData) {
+    const averageData = studentData.map((studentItem) => {
+      const average = (studentItem.difficult + studentItem.fun) / 2;
+      return {
+        id: studentItem.id,
+        name: studentItem.name,
+        assignment: studentItem.assignment,
+        average: average,
+      };
+    });
+    return averageData;
+  }
+
+
+  render() {
+    const studentData = this.props.studentData.filter((item) => {
+      return item.name === this.state.studentName;
+    });
+
+    return (
+      <div className="StudentPage">
+        <h1>Student: {this.state.studentName}</h1>
+        <ChartComponent studentData={studentData} />
+         />
+      </div>
+    );
+  }
+}
+
+export default StudentPage;
